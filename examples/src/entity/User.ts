87c50d5483b042c64b57f0525070764be21a7300
Roman <ceoro9@protonmail.com>
2020-11-08 18:46:01 +0300

    docs(examples): add encrypted column example

--- examples/src/entity/User.ts
+++ examples/src/entity/User.ts
@@ -1,9 +1,25 @@
 import { Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { EncryptedColumnProtection } from '../../../src';
 
-const encryptedColumnOptions = {
+const hashedColumnOptions = {
   hash: {
     algorithm: 'sha256',
+  },
+};
+
+const slaveKey = '073ECDE5C7D45D9C6D4727C37818F92B';
+
+const encryptedColumnOptions = {
+  encrypt: {
+    algorithm: 'AES-256-CBC',
+    ivLength: 16,
+    masterKeyId: 'super_secret_master_key_id',
+    cipheredSlaveKey: Buffer.from('ff01ff', 'hex'),
+    slaveKey: Buffer.from(slaveKey),
+    decryptSlaveKey: (cipheredSlaveKey: Buffer, masterKeyId: string) => {
+      console.log('decrypting slave key :: ', cipheredSlaveKey, masterKeyId);
+      return Buffer.from(slaveKey);
+    },
   },
 };
 
@@ -12,7 +28,7 @@ export class User {
   @PrimaryGeneratedColumn()
   public id: number;
 
-  @EncryptedColumnProtection(encryptedColumnOptions)
+  @EncryptedColumnProtection(hashedColumnOptions)
   public firstName: string;
 
   @EncryptedColumnProtection(encryptedColumnOptions)
